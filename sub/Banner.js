355f2b930bb33e88a6c8b9eec433ceb4b150d724
Alwayne <90293555+baileyalo@users.noreply.github.com>
2024-08-04 09:11:00 -0400

    update profile pic

--- sub/Banner.js
+++ sub/Banner.js
@@ -56,7 +56,7 @@ export default function Banner() {
         </section>
         <a target="_blank" href="https://www.linkedin.com/in/alwayne-bailey/">
           <figure className={styles.bannerContRight}>
-            <img alt="Alwayne Bailey" src="https://green-difficult-vulture-434.mypinata.cloud/ipfs/QmbK992cBygK3FSwYMjCGAKD5823k2XD3pceBDCvLowYJe" />
+            <img alt="Alwayne Bailey" src="../public/profile_pic.png" />
           </figure>
         </a>
         <div className={`${styles.buttons}`}>
