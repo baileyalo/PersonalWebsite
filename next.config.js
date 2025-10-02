73839e06bb18108a50ba2b626200d85de71dbf1d
Axelle Bangala <axellebangala@Axelles-MacBook-Air.local>
2025-10-01 21:15:37 -0400

    update build

--- next.config.js
+++ next.config.js
@@ -4,16 +4,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-  domains: ["green-difficult-vulture-434.mypinata.cloud"],
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'green-difficult-vulture-434.mypinata.cloud',
-      port: '',
-      pathname: '/ipfs/**',
-    },
-  ],
-},
+    unoptimized: true,
+    domains: ["green-difficult-vulture-434.mypinata.cloud"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'green-difficult-vulture-434.mypinata.cloud',
+        port: '',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
   env: {
     NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
     NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
