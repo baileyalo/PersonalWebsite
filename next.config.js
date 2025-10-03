46dd79d9a960588de75a236522837d339fdf9175
Axelle Bangala <axellebangala@Axelles-MacBook-Air.local>
2025-10-02 20:40:54 -0400

    update build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   // build allows API routes to function during runtime.
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true, // Add trailing slashes to help with Netlify routing
   images: {
     unoptimized: true,
     domains: ["green-difficult-vulture-434.mypinata.cloud"],
