549dda5d13bbbdb56caec83fd2ad365601a26f66
Axelle Bangala <axellebangala@Axelles-MacBook-Air.local>
2025-10-14 20:31:46 -0400

    adjust further styling

--- sub/NavBar.tsx
+++ sub/NavBar.tsx
@@ -46,7 +46,7 @@ export default function NavBar(): JSX.Element {
 
   return (
     <>
-      <nav className="top-0 left-0 z-[1001] block block mobile-sm:hidden">
+      <nav className="top-0 left-0 z-[1001] block mobile-sm:hidden sm:hidden">
         <Hamburger />
         <ul
           id="navBarResUl"
