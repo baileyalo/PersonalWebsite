3f35bd9e995cd461b42537d2992061fbdcfd291f
Al Bailey <albailey@Als-MacBook-Air.local>
2026-04-26 10:41:43 -0400

    improvements on UX

--- sub/About.tsx
+++ sub/About.tsx
@@ -14,8 +14,8 @@ export default function About(): JSX.Element {
             My name is Alwayne Bailey,
             <br />
             <span className="colorRed">
-              TechOps Engineer ● Software Engineer ● Web3 Engineer ● Technical Support Specialist ●
-              Customer Support ● Banker ● Teacher
+              TechOps Engineer • Software Engineer • Web3 Engineer • Technical Support Specialist •
+              Customer Support • Banker • Teacher
             </span>
           </h3>
         </header>
